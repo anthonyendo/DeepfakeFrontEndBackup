2317c1d1f85722b3d357f02a9b1aa0e1d2cbce8e
anthonyendo <112037833+anthonyendo@users.noreply.github.com>
2025-12-01 23:24:19 -0800

    Delete DeepFakeFrontEnd directory
    Need to reupload because accidentally uploaded the entire folder instead of individual files

--- DeepFakeFrontEnd/pages/1_About.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("About")
-st.write("This is the About page.")
--- DeepFakeFrontEnd/pages/2_Services.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Services")
-st.write("This is the Services page.")
